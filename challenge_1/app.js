16637f03d637e922654b42484fef56163f260941
Christopher Weilacker <chrisweilacker@gmail.com>
2019-01-04 20:40:24 -0800

    Completed Basic Game

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,110 @@
+var controller = {
+  resetBoard : function (event) {
+    model.board = [new Array(3), new Array(3), new Array(3)];
+    if (model.hasWon) {
+      model.currentTurn = model.winner;
+      model.hasWon = false;
+      model.winner = null;
+    } else {
+      model.currentTurn = 'X';
+    }
+    view.emptyBoard();
+  },
+  placePiece: function (event) {
+    model.changePiece(Number(event.target.id[0]), Number(event.target.id[1]));
+    view.renderPiece(event.target.id, model.board[Number(event.target.id[0])][Number(event.target.id[1])]);
+    view.renderAnnouncmentLabel(model.currentTurn, model.currentTurn === 'X' ? model.XPlayerName : model.OPlayerName, model.hasWon === false ? 0 : 1);
+    view.renderScoreBoard(model.scoreBoard);
+  },
+  changeOName: function (event){
 
+  },
+  changeXName: function (event) {
+
+  }
+}
+
+var view = {
+  renderAnnouncmentLabel: function (piece, name, type) {
+    if (type === 0) {
+      document.getElementById('announcement').innerText = `           It is ${piece}'s (${name}) turn`;
+    } else {
+      document.getElementById('announcement').innerText = `           ${piece} (${name}) has Won!!`;
+    }
+  },
+  renderScoreBoard: function (scores) {
+    document.getElementById('scoreboard').innerHTML = `Scoreboard<br />X: ${scores.X}<br /> O: ${scores.O}`
+  },
+  renderPiece: function (id, piece) {
+    if (piece) {
+      document.getElementById(id).innerHTML = '<P id="' + id +'P" style="font-size:80px; text-align:center;">' + piece + '</p>';
+    }
+
+  },
+  emptyBoard: function () {
+    var pieces = document.getElementsByTagName('td');
+
+    for (var i = 0; i<pieces.length; i++) {
+      pieces[i].innerHTML = '<P style="font-size:80px; text-align:center;"></p>';
+    }
+  }
+}
+
+var model = {
+  board: [new Array(3), new Array(3), new Array(3)],
+  scoreBoard: {X: 0, O: 0},
+  changePiece: function (row, column) {
+    console.log(row);
+    if (this.board[row][column]!==undefined) {
+      return;
+    }
+    if (!this.hasWon) {
+
+      this.board[row][column] = this.currentTurn;
+      this.checkBoard(row, column);
+    }
+
+    if (!this.hasWon) {
+      this.currentTurn === 'X' ? this.currentTurn = 'O' : this.currentTurn = 'X';
+    }
+
+  },
+  checkBoard: function (row, column) {
+    if (this.board[row][0] === this.board[row][1] && this.board[row][0] === this.board[row][2]) {
+      this.hasWon = true;
+      this.winner = this.currentTurn;
+      this.scoreBoard[this.currentTurn] ++;
+    } else if (this.board[0][column] === this.board[1][column] && this.board[0][column] === this.board[2][column]) {
+      this.hasWon = true;
+      this.winner = this.currentTurn;
+      this.scoreBoard[this.currentTurn] ++;
+    } else if (this.board[0][0] != undefined && this.board[0][0] === this.board[1][1] && this.board[0][0] === this.board[2][2]) {
+      this.hasWon = true;
+      this.winner = this.currentTurn;
+      this.scoreBoard[this.currentTurn] ++;
+    } else if (this.board[0][2] != undefined && this.board[0][2] === this.board[1][1] && this.board[0][2] === this.board[2][0]) {
+      this.hasWon = true;
+      this.winner = this.currentTurn;
+      this.scoreBoard[this.currentTurn] ++;
+    }
+  },
+  hasWon: false,
+  winner: null,
+  currentTurn: 'X',
+  XPlayerName: 'X',
+  OPlayerName: 'O'
+}
+
+
+var pieces = document.getElementsByTagName('td');
+
+for (var i = 0; i<pieces.length; i++) {
+  pieces[i].onclick = controller.placePiece;
+}
+
+document.getElementById('XPlayerName').onchange = controller.changeXName;
+
+document.getElementById('OPlayerName').onchange = controller.changeOName;
+
+document.getElementById('Reset').onclick = controller.resetBoard;
 
